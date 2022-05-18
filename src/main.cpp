a9fe4315c381222d1192252db3b54f27bd00f363
Nguyễn Trần Gia Bảo <100202780+onlyzabao@users.noreply.github.com>
2022-05-19 03:55:25 +0700

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,95 @@
 #include "../core/GameEngine.h"
 #include "../interface/GameUI.h"
 
-int playerNumber = MAX_PLAYERS_NUMER;
-
-std::vector<Coordinate> startPosition = {
-    {SCREEN_WIDTH / 4 - SPACESHIP_WIDTH / 2, (SCREEN_HEIGHT * 3) / 4 - SPACESHIP_HEIGHT / 2},
-    {(SCREEN_WIDTH * 3) / 4 - SPACESHIP_WIDTH / 2, (SCREEN_HEIGHT * 3) / 4 - SPACESHIP_HEIGHT / 2},
-    {SCREEN_WIDTH / 4 - SPACESHIP_WIDTH / 2, SCREEN_HEIGHT / 4 - SPACESHIP_HEIGHT / 2},
-    {(SCREEN_WIDTH * 3) / 4 - SPACESHIP_WIDTH / 2, SCREEN_HEIGHT / 4 - SPACESHIP_HEIGHT / 2},
-};
-
-std::vector<MovementControl> moveControl = {
-    {SDL_SCANCODE_W, SDL_SCANCODE_S, SDL_SCANCODE_A, SDL_SCANCODE_D},
-    {SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDL_SCANCODE_LEFT, SDL_SCANCODE_RIGHT},
-    {SDL_SCANCODE_Y, SDL_SCANCODE_H, SDL_SCANCODE_G, SDL_SCANCODE_J},
-    {SDL_SCANCODE_P, SDL_SCANCODE_SEMICOLON, SDL_SCANCODE_L, SDL_SCANCODE_APOSTROPHE}
-};
-
-ShootingControl shotControl =
+void runGuideMenu(GameUI *_p_gameUI)
 {
-    SDL_SCANCODE_R,
-    SDL_SCANCODE_SLASH,
-    SDL_SCANCODE_I,
-    SDL_SCANCODE_RIGHTBRACKET
-};
+    // Guide menu
+    _p_gameUI->initGuideMenuButton();
+    Status status = Status::BEGIN_STATUS;
+    while (status != Status::EXIT_STATUS && !_p_gameUI->isQuit())
+    {
+        _p_gameUI->handleGuideMenuEvent(status);
+        _p_gameUI->clear();
+        _p_gameUI->drawBackground();
+        _p_gameUI->drawGuideMenu(_p_gameUI->getButtonMap());
+        _p_gameUI->present();
+    }
+    _p_gameUI->initMainMenuButton();
+}
 
-void runOptionMenu(GameUI *_p_gameUI);
-void runMiniMenu(GameUI *_p_gameUI, Status &_status, const Player &_winner);
-void runGame(GameUI *_p_gameUI);
+void runMiniMenu(GameUI *_p_gameUI, Status &_status, const Player &_winner)
+{
+    // Mini menu & Winner display
+    _p_gameUI->initMiniMenuButton();
+    while ((_status == Status::END_STATUS || _status == Status::PAUSE_STATUS) && !_p_gameUI->isQuit())
+    {
+        _p_gameUI->handleMiniMenuEvent(_status);
+        _p_gameUI->clear();
+        _p_gameUI->drawBackground();
+        _p_gameUI->drawMiniMenu(_winner, _p_gameUI->getButtonMap());
+        _p_gameUI->present();
+    }
+}
+
+void runStartMenu(GameUI *_p_gameUI)
+{
+    _p_gameUI->initStartMenuButton();
+    Status status = Status::BEGIN_STATUS;
+    while (status != Status::END_STATUS && !_p_gameUI->isQuit())
+    {
+        _p_gameUI->handleStartMenuEvent(status);
+        _p_gameUI->clear();
+        _p_gameUI->drawBackground();
+        _p_gameUI->drawStartMenu(_p_gameUI->getButtonMap());
+        _p_gameUI->present();
+    }
+}
+
+void runGame(GameUI *_p_gameUI)
+{
+    runStartMenu(_p_gameUI);
+
+    GameEngine *p_gameEngine = new GameEngine(_p_gameUI->getPlayerNumber());
+    p_gameEngine->init(_p_gameUI->getMovementKey(), _p_gameUI->getShootingKey());
+
+    _p_gameUI->playMusic(Music::CHASING_MUSIC);
+    Status status = Status::RUNNING_STATUS;
+    while (status != Status::EXIT_STATUS && !_p_gameUI->isQuit())
+    {
+        _p_gameUI->handlePlaygroundEvent(status);
+        if (status == Status::RUNNING_STATUS)
+        {
+            // Playing game
+            p_gameEngine->handleMovement(_p_gameUI);
+            p_gameEngine->handleCollision(_p_gameUI);
+            p_gameEngine->draw(_p_gameUI);
+            p_gameEngine->updateStatus(status);
+        }
+        else if (status == Status::BEGIN_STATUS)
+        {
+            // Restart game
+            delete p_gameEngine;
+            p_gameEngine = new GameEngine(_p_gameUI->getPlayerNumber());
+            p_gameEngine->init(_p_gameUI->getMovementKey(), _p_gameUI->getShootingKey());
+            _p_gameUI->playMusic(Music::CHASING_MUSIC);
+            status = Status::RUNNING_STATUS;
+        }
+        else if (status == Status::END_STATUS || status == Status::PAUSE_STATUS)
+        {
+            // Pause & End game
+            _p_gameUI->pauseMusic();
+            runMiniMenu(_p_gameUI, status, p_gameEngine->getWinner());
+            _p_gameUI->resumeMusic();
+        }
+    }
+    delete p_gameEngine;
+    _p_gameUI->stopMustic();
+    _p_gameUI->initMainMenuButton();
+}
 
 int main(int argc, char *argv[])
 {
+    // Initialize user interface
     GameUI gameUI;
     GameUI *const p_gameUI = &gameUI;
     if (p_gameUI->init())
@@ -41,19 +100,23 @@ int main(int argc, char *argv[])
             p_gameUI->initMainMenuButton();
             while (!p_gameUI->isQuit())
             {
+                // Main menu
                 p_gameUI->handleMainMenuEvent(status);
                 if (status == Status::BEGIN_STATUS)
                 {
+                    // Playground
                     runGame(p_gameUI);
                     status = Status::NONE_STATUS;
                 }
-                else if (status == Status::OPTION_STATUS)
+                else if (status == Status::GUIDE_STATUS)
                 {
-                    runOptionMenu(p_gameUI);
+                    // Setting
+                    runGuideMenu(p_gameUI);
                     status = Status::NONE_STATUS;
                 }
                 else if (status == Status::EXIT_STATUS)
                 {
+                    // Exit
                     p_gameUI->exit();
                 }
                 p_gameUI->clear();
@@ -66,53 +129,3 @@ int main(int argc, char *argv[])
     p_gameUI->free();
     return 0;
 }
-
-void runOptionMenu(GameUI *_p_gameUI)
-{
-
-}
-
-void runGame(GameUI *_p_gameUI)
-{
-    GameEngine *p_gameEngine = new GameEngine(playerNumber);
-    p_gameEngine->init(startPosition, moveControl, shotControl);
-
-    Status status = Status::RUNNING_STATUS;
-    while (status != Status::EXIT_STATUS && !_p_gameUI->isQuit())
-    {
-        _p_gameUI->handlePlaygroundEvent(status);
-        if (status == Status::RUNNING_STATUS)
-        {
-            p_gameEngine->handleMovement(_p_gameUI);
-            p_gameEngine->handleCollision(_p_gameUI);
-            p_gameEngine->draw(_p_gameUI);
-            p_gameEngine->updateStatus(status);
-        }
-        else if (status == Status::BEGIN_STATUS)
-        {
-            delete p_gameEngine;
-            p_gameEngine = new GameEngine(playerNumber);
-            p_gameEngine->init(startPosition, moveControl, shotControl);
-            status = Status::RUNNING_STATUS;
-        }
-        else if (status == Status::END_STATUS)
-        {
-            runMiniMenu(_p_gameUI, status, p_gameEngine->getWinner());
-        }
-    }
-    delete p_gameEngine;
-}
-
-void runMiniMenu(GameUI *_p_gameUI, Status &_status, const Player &_winner)
-{
-    _p_gameUI->initMiniMenuButton();
-    while (_status == Status::END_STATUS && !_p_gameUI->isQuit())
-    {
-        _p_gameUI->handleMiniMenuEvent(_status);
-        _p_gameUI->clear();
-        _p_gameUI->drawBackground();
-        _p_gameUI->drawMiniMenu(_winner, _p_gameUI->getButtonMap());
-        _p_gameUI->present();
-    }
-}
-
